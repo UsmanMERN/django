24b35c9a72209710da92debf681eed8b41b29cf0
UsmanMern <mu1363131@gmail.com>
2024-08-04 22:10:26 +0500

    really enjoying

--- learningdjango/learning/admin.py
+++ learningdjango/learning/admin.py
@@ -1,4 +1,29 @@
 from django.contrib import admin
-from .models import BookType
+from .models import BookType, BookCertificate, BookReview, Store
 
-admin.site.register(BookType)
+# admin.site.register(BookType)
+# admin.site.register(BookType)
+
+
+class BookReviewInLine(admin.TabularInline):
+    model = BookReview
+    extra = 2
+
+
+class BookTypesAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "date_added")
+    inlines = [BookReviewInLine]
+
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ("name", "location")
+    filter_horizontal = ("book_types",)
+
+
+class BookCertificateAdmin(admin.ModelAdmin):
+    list_display = ("book", "certificate_numer")
+
+
+admin.site.register(BookType, BookTypesAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(BookCertificate, BookCertificateAdmin)
--- learningdjango/learning/models.py
+++ learningdjango/learning/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class BookType(models.Model):
@@ -17,3 +18,45 @@ class BookType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# one to many
+
+
+class BookReview(models.Model):
+    book = models.ForeignKey(BookType, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    commet = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} review for {self.book.name}"
+
+
+# many to many
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    book_types = models.ManyToManyField(BookType, related_name="stores")
+
+    def __str__(self):
+        return self.name
+
+
+# one to one
+
+
+class BookCertificate(models.Model):
+    book = models.OneToOneField(
+        BookType, on_delete=models.CASCADE, related_name="certificate"
+    )
+    certificate_numer = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+
+    def __str__(self):
+        return f"Certificate for {self.book.name}"
